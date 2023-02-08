8e2433adc4387c19ec1f8f01c63a74c853f7851e
geoffwithers <geoffwithers91@gmail.com>
2023-02-08 14:01:59 -0700

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -11,4 +11,20 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) {
+    var criteria = prompt("Please select which criteria to include in your password:");
+    var length = prompt("Please choose a length for your password between 8 and 128 characters:");
+    var lowerCase = prompt("Please select if you'd like to include lower case letters:");
+    var upperCase = prompt("Please select if you'd like to include upper case letters:");
+    var numbers = prompt("Please select if you'd like to include numbers:");
+    var specialChar= prompt("Please select if you'd like to include special characters:");
+
+    console.log("")
+    console.log("Password length: " + length);
+    console.log("Lower case letters: " + lowerCase);
+    console.log("Upper case letters: " + upperCase);
+    console.log("Numbers: " + numbers);
+    console.log("Special characters: " + specialChar);
+} 
+
+
